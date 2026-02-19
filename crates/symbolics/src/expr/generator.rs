54fdecfe9d22cb2bd155e5351b00ad58e2a96976
Clemens Kerschbaumer <clemens.kerschbaumer@gmail.com>
2026-02-19 11:32:34 +0100

    Add quality of life functions: Generate expr for f,g,h,cos,sin,exp

--- crates/symbolics/src/expr/generator.rs
+++ crates/symbolics/src/expr/generator.rs
@@ -21,3 +21,27 @@ impl ExprBuilder for SymbolGenerator {
         Expr::new_symbol(&self.name)
     }
 }
+
+pub fn f() -> Expr<()> {
+    Expr::new_compound(Expr::new_symbol("f"), Vec::new())
+}
+
+pub fn g(a: Expr) -> Expr<()> {
+    Expr::new_compound(Expr::new_symbol("f"), vec![a])
+}
+
+pub fn h(a: Expr, b: Expr) -> Expr<()> {
+    Expr::new_compound(Expr::new_symbol("f"), vec![a, b])
+}
+
+pub fn cos(a: Expr) -> Expr<()> {
+    Expr::new_compound(Expr::new_symbol("Cos"), vec![a])
+}
+
+pub fn sin(a: Expr) -> Expr<()> {
+    Expr::new_compound(Expr::new_symbol("Sin"), vec![a])
+}
+
+pub fn exp(a: Expr) -> Expr<()> {
+    Expr::new_compound(Expr::new_symbol("Exp"), vec![a])
+}
