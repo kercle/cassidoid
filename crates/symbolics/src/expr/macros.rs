76bde5f74e5c38e47083183c361b52217dbcf3dd
Clemens Kerschbaumer <clemens.kerschbaumer@gmail.com>
2026-02-20 23:38:02 +0100

    Add more trigon rules: Sin/Cos well known values.

--- crates/symbolics/src/expr/macros.rs
+++ crates/symbolics/src/expr/macros.rs
@@ -4,3 +4,18 @@ macro_rules! symbol {
         ( $($crate::expr::generator::SymbolGenerator::new($x)),* )
     };
 }
+
+#[macro_export]
+macro_rules! chain_replace_quick_and_dirty {
+    // allow trailing comma
+    ($expr:expr, $({ $($pat:tt)* } => { $($rep:tt)* }),+ $(,)?) => {{
+        let mut __e = $expr;
+        $(
+            __e = __e.replace_all_quick_and_dirty(
+                norm_expr! { $($pat)* },
+                raw_expr!  { $($rep)* },
+            );
+        )+
+        __e
+    }};
+}
--- crates/symbolics/src/expr/simplify/trigon.rs
+++ crates/symbolics/src/expr/simplify/trigon.rs
@@ -1,47 +1,63 @@
 use expr_macro::{norm_expr, raw_expr};
 
-use crate::expr::{Expr, NormalizedExpr, atom::Atom, matcher::MatchIter, pattern::Pattern};
+use crate::{
+    chain_replace_quick_and_dirty,
+    expr::{Expr, NormalizedExpr, atom::Atom, matcher::MatchIter, pattern::Pattern},
+};
 
-pub fn replace_symbols(expr: Expr, s: &str, replacement: &Expr) -> Expr {
-    expr.map_bottom_up(&|e| {
-        if e.matches_symbol(s) {
-            replacement.clone()
-        } else {
-            e
-        }
-    })
-}
-
-// TODO: implement proper rewrite engine
-pub fn replace_all_quick_and_dirty(expr: Expr, norm_pattern_expr: NormalizedExpr, replacement: Expr) -> Expr {
-    let pattern_expr = norm_pattern_expr.take_expr();
-    let pattern = Pattern::from_expr(&pattern_expr);
-
-    expr.map_bottom_up(&|e| {
-        if let Some(ctx) = MatchIter::new(&e, &pattern).next() {
-            let mut new_term = replacement.clone();
-            for (k, v) in ctx.iter() {
-                new_term = replace_symbols(new_term, k, v.get_expr());
+impl Expr {
+    fn replace_symbols(self, s: &str, replacement: &Expr) -> Expr {
+        self.map_bottom_up(&|e| {
+            if e.matches_symbol(s) {
+                replacement.clone()
+            } else {
+                e
             }
-            new_term
-        } else {
-            e
-        }
-    })
+        })
+    }
+
+    // TODO: implement proper rewrite engine
+    fn replace_all_quick_and_dirty(
+        self,
+        norm_pattern_expr: NormalizedExpr,
+        replacement: Expr,
+    ) -> Expr {
+        let pattern_expr = norm_pattern_expr.take_expr();
+        let pattern = Pattern::from_expr(&pattern_expr);
+
+        self.map_bottom_up(&|e| {
+            if let Some(ctx) = MatchIter::new(&e, &pattern).next() {
+                let mut new_term = replacement.clone();
+                for (k, v) in ctx.iter() {
+                    new_term = new_term.replace_symbols(k, v.get_expr());
+                }
+                new_term
+            } else {
+                e
+            }
+        })
+    }
 }
 
 pub fn simplify_trigon(expr: Expr) -> Expr {
-    let expr = replace_all_quick_and_dirty(
-        expr,
-        norm_expr! { Sqrt[1 - Cos[Pattern[x, Blank[]]]^2] },
-        raw_expr! { Sin[x]^2 },
-    );
+    chain_replace_quick_and_dirty!(expr,
+        { Sin[0] }       => { 0 },
+        { Sin[pi / 12] } => { Sqrt[2] * (Sqrt[3] - 1) / 4 },
+        { Sin[pi / 10] } => { (Sqrt[5] - 1) / 4 },
+        { Sin[pi / 8] }  => { (Sqrt[2 - Sqrt[2]]) / 2 },
+        { Sin[pi / 6] }  => { 1/2 },
+        { Sin[pi / 5] }  => { (Sqrt[2] * (Sqrt[5 - Sqrt[5]])) / 4 },
+        { Sin[pi / 4] }  => { 1/Sqrt[2] },
 
-    let expr = replace_all_quick_and_dirty(
-        expr,
-        norm_expr! { Sqrt[1 - Sin[Pattern[x, Blank[]]]^2] },
-        raw_expr! { Cos[x]^2 },
-    );
+        { Cos[0] }       => { 1 },
+        { Cos[pi / 12] } => { Sqrt[2] * (Sqrt[3] + 1) / 4 },
+        { Cos[pi / 10] } => { (Sqrt[2] * (Sqrt[5 + Sqrt[5]])) / 4 },
+        { Cos[pi / 8] }  => { (Sqrt[2 + Sqrt[2]]) / 2 },
+        { Cos[pi / 6] }  => { Sqrt[3] / 2 },
+        { Cos[pi / 5] }  => { (Sqrt[5] + 1) / 4 },
+        { Cos[pi / 4] }  => { 1/Sqrt[2] },
 
-    expr
+        { Sqrt[1 - Cos[Pattern[x, Blank[]]]^2] } => { Sin[x]^2 },
+        { Sqrt[1 - Sin[Pattern[x, Blank[]]]^2] } => { Cos[x]^2 },
+    )
 }
